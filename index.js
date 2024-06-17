4918af8cdbc89e438a38d50e726a7e96071c42c0
Dark <exo.bussness@outlook.com>
2024-06-17 19:47:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -12,21 +12,21 @@ function rollDice() {
         const value = Math.floor(Math.random() * 6 + 1);
         values.push(value);
         images.push(`<img src="photos/${value}.png">`);
-
     }
 
-    if (diceNumber == '') {
+    if (diceNumber === '' ) {
 
         diceResult.textContent = 'No value found , please try adding a value.';
         diceImages.textContent = " "
-        console.log("None value found");
+        console.log("No value found");
 
     }
     else {
 
         diceResult.textContent = `Dice : ${values.join(", ")}`;
         diceImages.innerHTML = images.join('');
+        console.log(diceNumber)
 
     }
-
+    
 }
