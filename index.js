d37e2f75828570cd61d6aca1959f84ae74b3e2bb
Dark <exo.bussness@outlook.com>
2024-06-17 19:30:51 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function rollDice(){
+function rollDice() {
 
     let diceNumber = document.getElementById("diceNumber").value;
     let diceResult = document.getElementById("diceResult");
@@ -7,26 +7,26 @@ function rollDice(){
     values = [];
     images = [];
 
-for(let i = 0; i < diceNumber; i++){
+    for (let i = 0; i < diceNumber; i++) {
 
-    const value = Math.floor(Math.random() * 6 + 1);
-    values.push(value);
-    images.push(`<img src="/photos/${value}.png">`);
+        const value = Math.floor(Math.random() * 6 + 1);
+        values.push(value);
+        images.push(`<img src="/photos/${value}.png">`);
 
-}
+    }
 
-if(diceNumber == '') {
+    if (diceNumber == '') {
 
-    diceResult.textContent = 'No value found , please try adding a value.';
-    diceImages.textContent = " "
-    console.log("None value found");
+        diceResult.textContent = 'No value found , please try adding a value.';
+        diceImages.textContent = " "
+        console.log("None value found");
 
-}
-else{
+    }
+    else {
 
-    diceResult.textContent = `Dice : ${values.join(", ")}`;
-    diceImages.innerHTML = images.join('');
+        diceResult.textContent = `Dice : ${values.join(", ")}`;
+        diceImages.innerHTML = images.join('');
 
-}
+    }
 
 }
